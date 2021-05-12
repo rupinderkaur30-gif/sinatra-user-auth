33b5ba8136c7089823ae1f883d3beb11a51424b8
rupinderkaur30-gif <rupi063094@gmail.com>
2021-05-12 17:59:57 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
 
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
